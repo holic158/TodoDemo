890ff5063b5c468e61bc26e96ecf5bb3682fef79
youngmin <holic158@naver.com>
2023-02-04 01:46:30 +0900

    Merge branch 'master' of https://gitlab.com/holic158/runner-demo
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml
    #	demo/build.gradle
    #	demo/src/main/java/com/example/demo/DemoApplication.java
    #	demo/src/main/resources/application.properties
    #	demo/src/test/java/com/example/demo/DemoApplicationTests.java

---
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private String username; // ID. ex Email
+    private String password; // password
+    private String role; // user role
+    private String authProvider; // User Info provider ex) github
+}
